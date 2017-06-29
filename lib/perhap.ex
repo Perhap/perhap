b9399f2279244dbfc46b4653f4e5d6e17e4308a2
Rob Martin <rob@version2beta.com>
2017-06-29 11:37:20 -0600

    vim detritus

---
+++ lib/perhap.ex
@@ -0,0 +1,18 @@
+defmodule Perhap do
+  @moduledoc """
+  Documentation for Perhap.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Perhap.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/perhap_test.exs
@@ -0,0 +1,8 @@
+defmodule PerhapTest do
+  use ExUnit.Case
+  doctest Perhap
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
