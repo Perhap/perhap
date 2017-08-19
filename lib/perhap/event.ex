72abe2fabeed4884d46189682e4f9654e7a9fd13
Rob Martin <rob@version2beta.com>
2017-08-18 22:39:46 -0600

    UUIDs and their manipulation

---
+++ lib/perhap/event.ex
@@ -0,0 +1,89 @@
+defmodule Perhap.Event do
+
+  @type t :: %__MODULE__{ event_id: String.t,
+                          data: map(),
+                          metadata: Perhap.Event.Metadata.t
+                        }
+  defstruct event_id: "",
+            data: %{},
+            metadata: %Perhap.Event.Metadata{}
+
+  @uuid_v1_regex    "[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}"
+  @time_order_regex "[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{12}"
+
+  # Timestamps and unique integers
+  @spec timestamp() :: integer
+  def timestamp(), do: System.system_time(:microseconds)
+  
+  @spec unique_integer() :: integer
+  def unique_integer(), do: System.unique_integer([:monotonic])
+
+  # UUID v1
+  @spec get_uuid_v1() :: String.t
+  def get_uuid_v1() do
+    { uuid, _state } = :uuid.get_v1(:uuid.new(self(), :erlang))
+    :uuid.uuid_to_string(uuid)
+  end
+
+  @spec is_uuid_v1?(charlist() | binary()) :: true | false
+  def is_uuid_v1?(nil), do: false
+  def is_uuid_v1?(input) when is_list(input), do: is_uuid_v1?(to_string(input))
+  def is_uuid_v1?(input) when is_binary(input) do
+    try do
+      :uuid.is_v1(:uuid.string_to_uuid(input))
+    catch
+      :exit, _ -> false
+    end
+  end
+
+  @spec uuid_v1_to_time_order(charlist() | binary()) :: String.t
+  def uuid_v1_to_time_order(uuid_v1) when is_list(uuid_v1) do
+    uuid_v1_to_time_order(to_string(uuid_v1))
+  end
+  def uuid_v1_to_time_order(uuid_v1) when is_binary(uuid_v1) do
+    [time_low, time_mid, time_high, node_hi, node_low] = String.split(uuid_v1, "-")
+    time_high <> "-" <> time_mid <> "-" <> time_low <> "-" <> node_hi <> "-" <> node_low
+  end
+
+  @spec time_order_to_uuid_v1(charlist() | binary()) :: String.t
+  def time_order_to_uuid_v1(time_order_uuid_v1) when is_list(time_order_uuid_v1) do
+    time_order_to_uuid_v1(to_string(time_order_uuid_v1))
+  end
+  def time_order_to_uuid_v1(time_order_uuid_v1) when is_binary(time_order_uuid_v1) do
+    [time_high, time_mid, time_low, node_hi, node_low] = String.split(time_order_uuid_v1, "-")
+    time_low <> "-" <> time_mid <> "-" <> time_high <> "-" <> node_hi <> "-" <> node_low
+  end
+
+  @spec extract_uuid_v1_time(charlist() | binary()) :: integer()
+  def extract_uuid_v1_time(input) when is_binary(input), do: extract_uuid_v1_time(to_charlist(input))
+  def extract_uuid_v1_time(input) when is_list(input) do
+    uuid = case is_time_order?(input) do
+      true -> input |> time_order_to_uuid_v1
+      _ -> input
+    end
+    uuid |> :uuid.string_to_uuid |> :uuid.get_v1_time
+  end
+
+  @spec is_time_order?(charlist() | binary()) :: true | false
+  def is_time_order?(input) when is_list(input), do: is_time_order?(to_string(input))
+  def is_time_order?(input) when is_binary(input) do
+    Regex.match?(~r/#{@time_order_regex}/, input)
+  end
+
+  # UUID v4
+  @spec get_uuid_v4() :: String.t
+  def get_uuid_v4() do
+    :uuid.get_v4() |> :uuid.uuid_to_string
+  end
+
+  @spec is_uuid_v4?(charlist()|binary()) :: true|false
+  def is_uuid_v4?(nil), do: false
+  def is_uuid_v4?(input) when is_list(input), do: is_uuid_v4?(to_string(input))
+  def is_uuid_v4?(input) when is_binary(input) do
+    try do
+      :uuid.is_v4(:uuid.string_to_uuid(input))
+    catch
+      :exit, _ -> false
+    end
+  end
+end
---
+++ test/perhap/event_test.exs
@@ -0,0 +1,55 @@
+defmodule Perhap.EventTest do
+  use ExUnit.Case, async: true
+  alias Perhap.Event
+
+  defp make_v1() do
+    {uuid, _} = :uuid.get_v1(:uuid.new(self()))
+    uuid |> :uuid.uuid_to_string |> to_string()
+  end
+
+  defp make_v4() do
+    :uuid.get_v4() |> :uuid.uuid_to_string |> to_string()
+  end
+
+  test "timestamp returns system time in microseconds" do
+    assert_in_delta(Event.timestamp(), :erlang.system_time(:microsecond), 10)
+  end
+
+  test "unique_integer returns monotonically increasing integers" do
+    unique_integers = for _n <- 1..10, do: Event.unique_integer()
+    assert unique_integers == Enum.sort(unique_integers |> Enum.dedup)
+  end
+
+  test "get_uuid_v1 returns valid uuid_v1" do
+    assert :uuid.is_v1(Event.get_uuid_v1() |> :uuid.string_to_uuid)
+  end
+
+  test "knows a uuid_v1 when it sees one" do
+    assert Event.is_uuid_v1?(make_v1())
+  end
+
+  test "flips the time so it can be sorted, and back again" do
+    uuid = make_v1()
+    [ulow, umid, uhigh, _, _] = String.split(uuid, "-")
+    flipped = Event.uuid_v1_to_time_order(uuid)
+    [fhigh, fmid, flow, _, _] = String.split(flipped, "-")
+    double_flipped = Event.time_order_to_uuid_v1(flipped)
+    refute uuid == flipped
+    assert uuid == double_flipped
+    assert {ulow, umid, uhigh} == {flow, fmid, fhigh}
+  end
+
+  test "extract datetime returns the time the event was created" do
+    uuid_time = make_v1() |> Event.extract_uuid_v1_time
+    system_time = System.system_time(:microsecond)
+    assert_in_delta(system_time, uuid_time, 100_000)
+  end
+
+  test "returns a valid uuid_v4" do
+    assert :uuid.is_v4(Event.get_uuid_v4() |> :uuid.string_to_uuid())
+  end
+
+  test "knows a valid uuid_v4 when it sees one" do
+    assert Event.is_uuid_v4?(make_v4())
+  end
+end
