7c34600d12d3dd444dc450ca1dbc3b1ed922db83
Rob Martin <rob@version2beta.com>
2017-08-22 22:56:50 -0600

    Configure libcluster

--- config/config.exs
+++ config/config.exs
@@ -14,3 +14,11 @@ config :logger, :access_log,
 config :logger, :error_log,
   metadata: [:application, :module, :function, :file, :line],
   level: :error
+
+config :libcluster,
+  topologies: [
+    perhap: [ strategy: Cluster.Strategy.Epmd,
+              config: [hosts: [:"perhap1@127.0.0.1"] ]]
+  ]
+
+config :swarm, debug: true
